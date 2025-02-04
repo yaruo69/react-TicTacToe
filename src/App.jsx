23e77429950c3009202fec3c9d650486ae0f2ed3
window-yaruo <shusaku691998@gmail.com>
2025-02-04 16:59:10 +0900

    stateのリフトアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
 import { useState } from "react";
 
 function Board() {
+  // 親コンポーネントのBoardでuseStateすることで、九個の盤面の状態をまとめて管理が可能。
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // イベントを処理するハンドラ関数の定義には handleSomething という名前を使う
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    // setSquaresをすることで、コンポーネントが再レンダーされる。
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+// イベントを表す props には onSomething という名前を使う
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
